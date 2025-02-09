69c45f4b45a11999ea062a39127a591adcaf0173
Logano04 <loganmolsen04@gmail.com>
2025-02-08 23:45:04 -0700

    stuff

---
+++ coolpics/coolpics.js
@@ -0,0 +1,1 @@
+let menuItems = document.querySelector("menu");
---
+++ w4ponder/debugging.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+
+function circleArea(radius) {
+  return radius * radius * PI;
+}
+
+area = circleArea(radius);
+console.log("area is " + area);
